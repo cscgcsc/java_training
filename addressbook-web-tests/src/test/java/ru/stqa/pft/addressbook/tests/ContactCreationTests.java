cd5676c75bdf806029d1d1c72c4acecde8fd15c7
cscgcsc <alex-groovy@mail.ru>
2021-03-14 19:56:20 +0200

    Exercise 13. Test data provider

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,18 +1,59 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Contact;
 import ru.stqa.pft.addressbook.model.Contacts;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class ContactCreationTests extends TestBaseAuth {
 
-    @Test
-    public void testContactCreation() {
+    @DataProvider
+    public Iterator<Object[]> validContactsFromJson() throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader("src/test/resources/contacts.json"));
+        String json = "";
+        String line = fileReader.readLine();
+        while(line != null) {
+            json += line;
+            line = fileReader.readLine();
+        }
+        Gson gson = new Gson();
+        List<Contact> contacts = gson.fromJson(json, new TypeToken<List<Contact>>(){}.getType()); //List<Contact>.class
+        return contacts.stream().map((contact) -> new Object[] {contact}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validContactsFromXml() throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader("src/test/resources/contacts.xml"));
+        String xml = "";
+        String line = fileReader.readLine();
+        while(line != null) {
+            xml += line;
+            line = fileReader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(Contact.class);
+        List<Contact> contacts = (List<Contact>) xStream.fromXML(xml);
+        return contacts.stream().map((contact) -> new Object[] {contact}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validContactsFromXml")
+    public void testContactCreation(Contact newContact) {
         app.contact.goToHomePage();
         Contacts before = app.contact.getAll();
-        Contact newContact = new Contact("Text 1", "Text 2").setMiddlename("Text 3").setEmail("Text 4").setEmail2("Text 5").setHomePhone("Text 6").setMobilePhone("Text 7").setAddress("Text 8").setBday("5").setBmonth("April").setByear("1990");
+        //Contact newContact = new Contact("Text 1", "Text 2").setMiddlename("Text 3").setEmail("Text 4").setEmail2("Text 5").setHomePhone("Text 6").setMobilePhone("Text 7").setAddress("Text 8").setBday("5").setBmonth("April").setByear("1990");
         app.contact.create(newContact);
         //verification
         Contacts after = app.contact.getAll();
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,18 +1,58 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.reflect.TypeToken;
+import com.thoughtworks.xstream.XStream;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.Group;
 import ru.stqa.pft.addressbook.model.Groups;
+
+import java.io.*;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.google.gson.Gson;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class GroupCreationTests extends TestBaseAuth {
 
-    @Test
-    public void testGroupCreation() {
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromJson() throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader("src/test/resources/groups.json"));
+        String json = "";
+        String line = fileReader.readLine();
+        while(line != null) {
+            json += line;
+            line = fileReader.readLine();
+        }
+        Gson gson = new Gson();
+        List<Group> groups = gson.fromJson(json, new TypeToken<List<Group>>(){}.getType()); //List<Group>.class
+        return groups.stream().map((group) -> new Object[] {group}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validGroupsFromXml() throws IOException {
+        BufferedReader fileReader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
+        String xml = "";
+        String line = fileReader.readLine();
+        while(line != null) {
+            xml += line;
+            line = fileReader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(Group.class);
+        List<Group> groups = (List<Group>) xStream.fromXML(xml);
+        return groups.stream().map((group) -> new Object[] {group}).collect(Collectors.toList()).iterator();
+    }
+
+    @Test(dataProvider = "validGroupsFromJson")
+    public void testGroupCreation(Group newGroup) {
         app.group.goToGroupPage();
         Groups before = app.group.getAll();
-        Group newGroup = new Group().setName("Text 1").setHeader("Text 2").setFooter("Text 3");
+        //Group newGroup = new Group().setName("Text 1").setHeader("Text 2").setFooter("Text 3");
         app.group.create(newGroup);
         //verification
         Groups after = app.group.getAll();
